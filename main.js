498bf453ae93489b808876832ebed3b18e3cec1b
Akaehomen peace <peacedavid049@gmail.com>
2023-09-25 17:05:45 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const adviceId = document.getElementById("advice-id");
+const adviceText = document.getElementById("advice-text");
+const adviceBtn = document.getElementById("generate-btn");
+const url = "https://api.adviceslip.com";
+
+async function getAdvice() {
+  const res = await fetch(url);
+  const { slip: {id, advice} } = await res.json();
+  adviceId.innerText = id;
+  adviceText.innerText = advice;
+}
+getAdvice()
+
+adviceBtn.addEventListener("click".getAdvice)
